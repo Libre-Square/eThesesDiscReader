ea4e17485539d16249815c21666bccd0580a8f2e
Alex Chan <alexchanwk@gmail.com>
2017-10-13 12:24:09 +0800

    Use net use command

--- eThesesDiscReader/Utils/DriveHelper.cs
+++ eThesesDiscReader/Utils/DriveHelper.cs
@@ -1,33 +1,61 @@
-﻿using IWshRuntimeLibrary;
+﻿//using IWshRuntimeLibrary;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace eThesesDiscReader.Utils
 {
     public class DriveHelper
     {
-        public static readonly string STORAGE_DRIVE_LETTER = "Y";
         public static readonly string STORAGE_DRIVE_PATH = @"\\rsdi-ns01.lib.cuhk.edu.hk\etd";
         public static readonly string STORAGE_DRIVE_USER_ID = @"etd_admin";
         public static readonly string STORAGE_DRIVE_PASSWORD = @"etd9@ss";
 
-        private static WshNetwork networkShell = new WshNetwork();
+        //private static WshNetwork networkShell = new WshNetwork();
 
         public static void connectStorageDrive()
         {
             disconnectStorageDrive();
-            networkShell.MapNetworkDrive(STORAGE_DRIVE_LETTER + ":", STORAGE_DRIVE_PATH, false, STORAGE_DRIVE_USER_ID, STORAGE_DRIVE_PASSWORD);
+
+            Process p = null;
+            try
+            {
+                p = Process.Start("cmd.exe", "/C net use " + STORAGE_DRIVE_PATH + STORAGE_DRIVE_PASSWORD + " /USER:" + STORAGE_DRIVE_USER_ID);
+                while (!p.WaitForExit(1000))
+                {
+                    //wait
+                }
+                if (p.ExitCode != 0)
+                    MessageBox.Show("Failed to connect network drive!", "Warn", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                if (p != null)
+                    p.Close();
+            }
         }
 
         public static void disconnectStorageDrive()
         {
-            DriveInfo driveInfo = new DriveInfo(STORAGE_DRIVE_LETTER);
-            if (driveInfo.IsReady)
-                networkShell.RemoveNetworkDrive(STORAGE_DRIVE_LETTER + ":", true, true);
+            Process p = null;
+            try
+            {
+                p = Process.Start("cmd.exe", "/C net use " + STORAGE_DRIVE_PATH + " /DELETE");
+                while (!p.WaitForExit(1000))
+                {
+                    //wait
+                }
+            }
+            finally
+            {
+                if (p != null)
+                    p.Close();
+            }
         }
 
         public static DriveInfo getDriveByType(DriveType driveType)
